9f7da8ec7871feb46bead43aa3ade040860cb5b0
Rasim Huziahmetov <rasim.huziahmetov@calaton.systems>
2020-09-04 15:49:11 +0500

    add build in repo, because netlify don't build

---
+++ build/precache-manifest.7eb966fb318c4582e8e9e727e5bebb51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6db02f0260a49753bd2dc48db0622bfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f2c1fdfe0eee1cbf820",
+    "url": "/static/css/main.7d972f51.chunk.css"
+  },
+  {
+    "revision": "62d228d76c29f7762d95",
+    "url": "/static/js/2.9abda4bc.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/static/js/2.9abda4bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f2c1fdfe0eee1cbf820",
+    "url": "/static/js/main.4bb78479.chunk.js"
+  },
+  {
+    "revision": "4a42fdcbdfd3cf55fea3",
+    "url": "/static/js/runtime-main.1c47a842.js"
+  }
+]);
